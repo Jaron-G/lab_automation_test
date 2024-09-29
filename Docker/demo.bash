a64f166acdb5c2dfbf50939e5bcbee8d82eec805
Jaron-G <2205250117@qq.com>
2024-09-29 19:04:06 +0800

    update Dockerfile

--- Docker/demo.bash
+++ Docker/demo.bash
@@ -5,13 +5,20 @@ docker run --rm -it \
     --env="DISPLAY=$DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --env="XAUTHORITY=$XAUTH" \
+    -e NVIDIA_DRIVER_CAPABILITIES=all \
     --volume="/home/$USER/catkin_ws/src/robotiq_gripper:/catkin_ws/src/robotiq_gripper" \
     --volume="/home/$USER/catkin_ws/src/ur10e_gripper:/catkin_ws/src/ur10e_gripper" \
     --volume="/home/$USER/catkin_ws/src/ur10e_gripper_moveit:/catkin_ws/src/ur10e_gripper_moveit" \
     --volume="/home/$USER/catkin_ws/src/lab_automation_test:/catkin_ws/src/lab_automation_test" \
+    --volume="/home/$USER/catkin_ws/src/grasp_eyeinhand:/catkin_ws/src/grasp_eyeinhand" \
+    --volume="/home/$USER/catkin_ws/src/eyeinhand_pkg/detect_pose_pkg:/catkin_ws/src/detect_pose_pkg" \
+    --volume="/home/$USER/catkin_ws/src/eyeinhand_pkg/calc_pose_pkg:/catkin_ws/src/calc_pose_pkg" \
+    --volume="/home/$USER/catkin_ws/src/eyeinhand_pkg/grasp_object_pkg:/catkin_ws/src/grasp_object_pkg" \
     --volume="/dev/bus/usb:/dev/bus/usb" \
     --net=host \
     --privileged \
-    ros-noetic-industrial-grasp-demo \
+    --runtime=nvidia \
+    --gpus all \
+    ros-noetic-lab-automation-demo \
     bash
 echo "Done."
